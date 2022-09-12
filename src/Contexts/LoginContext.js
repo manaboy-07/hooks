752b4e5cadf1968b9f084c0b2e737385d143cba2
manaboy-07 <manassehoruebor@gmail.com>
2022-09-12 22:30:04 +0100

    added more instructions

--- src/Contexts/LoginContext.js
+++ src/Contexts/LoginContext.js
@@ -8,4 +8,4 @@ export const LoginContext = createContext({});
 // Step 1. Create a Context using createCOntext
 // Step 2. in the parent file use it to wrap the sibling components use the name and adding .provider
 // Step 3. assign a value to it , it can be anything preferably useState
-// Step 4. 
+// Step 4. in the child component desstructure what you want to use.
